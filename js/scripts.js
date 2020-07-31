017e690a909691a1cb947e5b6d91c10463e3ce5b
George Wayne Mwangi <waynegeorge193.com>
2020-07-31 10:04:33 +0300

    animate footer to appear from top and bottom when page is loaded.

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,17 @@ sr.reveal ('.animate-right', {
   delay: 300
 
 });
+sr.reveal ('.animate-top', {
+  origin:'top',
+  duration:1000,
+  distance:'25rem',
+  delay: 600
+
+});
+sr.reveal ('.animate-bottom', {
+  origin:'bottom',
+  duration:1000,
+  distance:'25rem',
+  delay: 600
+
+});
