dc80e078d77af749ab3b4006a4a625892127a801
George Wayne Mwangi <waynegeorge193.com>
2020-08-01 10:54:22 +0300

    create form to collect pizza order.

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,24 @@ sr.reveal ('.animate-bottom', {
   delay: 600
 
 });
+$(document).ready(function() {
+  $("#add-new-pizza").click(function() {
+    $("#new-pizzas").append('<div class="new-pizza">' +
+                                 '<div class="form-group">' +
+                                   '<label for="choose-pizza-size">Choose pizza size</label>' +
+                                   '<input type="text" class="form-control pizza-size">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="choose-crust">Choose crust</label>' +
+                                   '<input type="text" class="form-control crust">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="choose-toppings">Choose toppings</label>' +
+                                   '<input type="text" class="form-control toppings">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="amount">Number of Pizzas</label>' +
+                                   '<input type="number" class="form-control number">' +
+                                 '</div>' +
+                               '</div>');
+  });
