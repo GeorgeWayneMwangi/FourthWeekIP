f06d2998a4dbf2c0ae65da08ea67a21b1a2fd980
George Wayne Mwangi <waynegeorge193.com>
2020-08-01 11:20:34 +0300

    create new js file for the pizza order page.

--- js/pizza-order.js
+++ js/pizza-order.js
@@ -1,43 +1,3 @@
-const selectElement=function(element) {
-  return document.querySelector(element);
-};
-
-let menuToggler= selectElement('.menu-toggle');
-let body= selectElement('body');
-
-menuToggler.addEventListener('click', function() {
-  body.classList.toggle('open');
-});
-
-window.sr = ScrollReveal();
-sr.reveal ('.animate-left', {
-  origin:'left',
-  duration:1000,
-  distance:'25rem',
-  delay: 300
-
-});
-sr.reveal ('.animate-right', {
-  origin:'right',
-  duration:1000,
-  distance:'25rem',
-  delay: 300
-
-});
-sr.reveal ('.animate-top', {
-  origin:'top',
-  duration:1000,
-  distance:'25rem',
-  delay: 600
-
-});
-sr.reveal ('.animate-bottom', {
-  origin:'bottom',
-  duration:1000,
-  distance:'25rem',
-  delay: 600
-
-});
 $(document).ready(function() {
   $("#add-new-pizza").click(function() {
     $("#new-pizzas").append('<div class="new-pizza">' +
@@ -59,3 +19,4 @@ $(document).ready(function() {
                                  '</div>' +
                                '</div>');
   });
+});
